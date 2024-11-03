5294ef4769d7e93ba2df0b264cd789317ed8c369
Ananth <ananthsa20@gmail.com>
2024-11-03 17:11:28 +0530

    Data Validation Completed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from textsummarization.pipeline.data_ingestion_pipeline import DataingestionTrainingPipeline
+from textsummarization.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from textsummarization.logging import logger
+
 
 
 STAGE_NAME='Data Ingestion Stage'
@@ -9,6 +11,17 @@ try:
     data_ingestion=DataingestionTrainingPipeline()
     data_ingestion.main()
     logger.info(f'>>>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME='Data Validation Stage'
+try:
+    logger.info(f'>>>>>  stage {STAGE_NAME} started <<<<<<<')
+    data_validation=DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f'>>>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x')
+    
 except Exception as e:
     logger.exception(e)
     raise e
--- src/textsummarization/config/congifuration.py
+++ src/textsummarization/config/congifuration.py
@@ -1,6 +1,6 @@
 from textsummarization.constants import *
 from textsummarization.utils.common import read_yaml,create_directories
-from textsummarization.entity import dataingestionConfig
+from textsummarization.entity import dataingestionConfig,DataValidationConfig
 
 
 class ConfigurationManager:
@@ -29,3 +29,18 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILE=config.ALL_REQUIRED_FILE
+            )
+
+        return data_validation_config
